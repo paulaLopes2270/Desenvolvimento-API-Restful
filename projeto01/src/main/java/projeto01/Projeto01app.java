149063cd29c7628e1b43b6d0bff365be4cba6e43
paulaLopes2270 <plnhschltz@gmail.com>
2021-05-27 11:53:01 -0300

    Primeiros projetos

---
+++ projeto01/src/main/java/projeto01/Projeto01app.java
@@ -0,0 +1,14 @@
+package projeto01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01app {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01app.class, args);
+
+	}
+
+}
